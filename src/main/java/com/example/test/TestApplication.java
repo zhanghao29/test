40406b29df99a873a26e9f44a3e6301132873e06
zhanghao <563109243@qq.com>
2019-06-16 19:06:39 +0800

    修改

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
-        // master 分支
+        // master 主线分支
         SpringApplication.run(TestApplication.class, args);
     }
 
