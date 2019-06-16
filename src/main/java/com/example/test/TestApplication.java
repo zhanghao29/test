899fc488124fa30f7aa8d74ac0b19d915e599ad3
zhanghao29 <46885395+zhanghao29@users.noreply.github.com>
2019-06-16 19:05:58 +0800

    Update TestApplication.java

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
-        // master 分支
+        // zhang 分支
         SpringApplication.run(TestApplication.class, args);
     }
 
